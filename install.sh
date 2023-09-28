7c23e196a5cb6bff4068496bf1458b80f755386d
Unknown INS <embebo672019@gmail.com>
2023-09-28 20:39:24 +0700

    update

--- install.sh
+++ install.sh
@@ -25,4 +25,6 @@ ln -s /usr/local/unknown/Unknown.sh /usr/local/bin/Unknown &> /dev/null
 
 ln -s /usr/local/unknown/UnknownCLI.sh /usr/local/bin/UnknownCLI &> /dev/null
 
+rm -rf Unknown-Manager-Openlitespeed &> /dev/null
+
 echo "----------------> Install Success.Use command 'Unknown' for use.Thank you"
