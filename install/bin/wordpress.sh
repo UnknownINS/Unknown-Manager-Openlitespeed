462fea496532517f75425a4e553ba1d866e319bf
UnknownINS <144177491+UnknownINS@users.noreply.github.com>
2024-01-09 15:03:00 +0700

    update

--- install/bin/wordpress.sh
+++ install/bin/wordpress.sh
@@ -29,8 +29,9 @@ wpCreateWebsite() {
     textRed "----------------> PLEASE CHECK DOMAIN AGAIN"
     exit
   fi
-  
-  nameDatabase=$(sed "s/\.-/_/g" <<<"$inputDomain")
+
+  nameDatabase=$(sed "s/\./_/g" <<<"$inputDomain")
+  nameDatabase=$(sed "s/-/_/g" <<<"$inputDomain")
 
   createDatabase $nameDatabase &>/dev/null
 
@@ -162,7 +163,8 @@ wpDeleteWebsite() {
 
   rm -rf $LSWS_VHOSTS/$inputDomain &>/dev/null
 
-  nameDatabase=$(sed "s/\.-/_/g" <<<"$inputDomain")
+  nameDatabase=$(sed "s/\./_/g" <<<"$inputDomain")
+  nameDatabase=$(sed "s/-/_/g" <<<"$inputDomain")
 
   deleteDatabase $nameDatabase &>/dev/null
 
@@ -251,9 +253,9 @@ wpRenameDomain() {
 
   textYellow "----------------> RENAME DATABASE"
 
-  databaseNewDomain=$(sed "s/\.-/_/g" <<<"$newDomain")
+  databaseNewDomain=$(sed "s/\./_/g" <<<"$newDomain")
 
-  databaseOldDomain=$(sed "s/\.-/_/g" <<<"$oldDomain")
+  databaseOldDomain=$(sed "s/-/_/g" <<<"$oldDomain")
 
   PRIVATE_SQL_PASSWORD_DOMAIN=$(openssl rand -base64 15);
 
@@ -339,7 +341,9 @@ rm -rf $UNKNOWN_DIR/$oldDomain/html
 
 mkdir -p $UNKNOWN_DIR/$oldDomain/html/
 
-nameDatabase=$(sed "s/\.-/_/g" <<<"$oldDomain")
+nameDatabase=$(sed "s/\./_/g" <<<"$oldDomain")
+
+nameDatabase=$(sed "s/-/_/g" <<<"$oldDomain")
 
 deleteDatabase $nameDatabase &>/dev/null
 
